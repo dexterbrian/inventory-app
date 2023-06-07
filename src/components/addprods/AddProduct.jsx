574020cdb881dbf98359220ffffdf0a6223f028a
Sylvia Mumbi <mumbi099@gmail.com>
2023-06-08 02:38:24 +0300

    Defined state variables

--- src/components/addprods/AddProduct.jsx
+++ src/components/addprods/AddProduct.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./AddProduct.css";
 
 function AddProduct() {
+    // step1: define state variables
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [category, setCategory] = useState("");
+    const [quantity, setQuantity] = useState("");
   return (
     <div>AddProduct</div>
   )
