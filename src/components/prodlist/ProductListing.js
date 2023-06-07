c9a2cfe2aa4882b03eea1e7ebc9fe593e1ff0f39
Sylvia Mumbi <mumbi099@gmail.com>
2023-06-07 16:45:28 +0300

    Render products in table

--- src/components/prodlist/ProductListing.js
+++ src/components/prodlist/ProductListing.js
@@ -12,8 +12,31 @@ function ProductListing() {
     });
 
 
+
 return (
-    <div>ProductListing</div>
+    <div>ProductListing
+      <table>
+        <thead>
+          <tr>
+            <th>NAME</th>
+            <th>DESCRIPTION</th>
+            <th>CATEGORY</th>
+            <th>QUANTITY</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>{product.description}</td>
+              <td>{product.category}</td>
+              <td>{product.quantity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    
   )
 }
 
