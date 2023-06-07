f59bf5dbfd8cdce47f095cf2b0a7d4124fc7bbc8
Future <futureworldzone@gmail.com>
2023-06-07 11:00:02 +0300

    Initial Commit to create Modify (Edit/Delete) Form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Modify from './Components/Modify/Modify';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Modify />
     </div>
   );
 }
