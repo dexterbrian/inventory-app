f3e0259ed6fec834fa5b46916bd39a17de77c634
Brian Waweru <brianwaweru627@gmail.com>
2023-06-08 02:37:59 +0300

    Added login, routing & navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Login from './components/Login';
+import { Route, Switch, useHistory } from 'react-router-dom/cjs/react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
+
+  const history = useHistory();
+  const [ isLoggedIn, setLoggedIn ] = useState(false);
+  const [ notification, setNotification ] = useState('');
+
   return (
     <div className="App">
       <h1>Inventory App</h1>
-      {/* {Our parent component will go here} */}
+      { isLoggedIn ? <NavBar /> : null }
+      <Switch>
+      <Route exact path="/">
+        {
+          isLoggedIn ? <h2>All Products</h2> : history.push('login')
+        }
+      </Route>
+      <Route exact path="/login">
+        <Login setLoggedIn={ setLoggedIn } setNotification={ setNotification }/>
+      </Route>
+      <Route exact path="/add">
+        <h2>Add Product</h2>
+      </Route>
+      <Route exact path="/edit">
+        <h2>Edit Product</h2>
+      </Route>
+    </Switch>
     </div>
   );
 }
