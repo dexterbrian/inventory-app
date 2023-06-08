d2167a6c0bb5a94334b53306181fb9390b2d61f7
Brian Waweru <brianwaweru627@gmail.com>
2023-06-08 04:29:53 +0300

    Added ProductListing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Inventory App</h1>
-      {/* {Our parent component will go here} */}
+      <ProductListing />
     </div>
   );
 }
