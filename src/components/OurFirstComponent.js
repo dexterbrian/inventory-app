53ee4b461fb6d1a208577544e1faa5392b242182
grace njeri wambui <gracenjeriwambui00@gmail.com>
2023-06-08 13:58:09 +0300

    sorting feature

--- src/components/OurFirstComponent.js
+++ src/components/OurFirstComponent.js
@@ -1,1 +1,13 @@
 // The code for our first component will go here
+import React from 'react'
+import SearchBox from './components';
+
+const OurFirstComponent = () => {
+  return (
+    <div>OurFirstComponent
+        <SearchBox />
+    </div>
+  )
+}
+
+export default OurFirstComponent
