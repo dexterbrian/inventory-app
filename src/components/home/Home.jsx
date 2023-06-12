2a6e3c0453f01e22c2b3e57402be2c10f5f478b3
Sylvia Mumbi <mumbi099@gmail.com>
2023-06-12 20:38:43 +0300

    Added logo and changed some properties

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import "./Home.css";
 
+
 function Home() {
   return (
     <div className='home' id='home'>
-       <h1 className='home-title'>Empowering Inventory Management</h1>
+       <h1 className='home-title'><span>Inventory made simple.</span> <br/>Get to streamline, optimize, succeed</h1>
        <h3 className='home-slogan'>Unlock Your Business Potential 
        with Invento: The Ultimate Inventory Management Solution for Streamlined
         Operations, Real-Time Insights, and Unwavering Success</h3>
-    </div>
-    
+    </div>   
   )
 }
 
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
+import logoImage from "../../images/logo2.png"
 
 function NavBar() {
-  return (
-    <nav className="navbar" id='navbar'>
-        <div className='navbar-container'>
-                <h1 className="logo">INVENTO</h1>
-            
-                <ul className="nav-links">
-                    <Link to="/"><li>Home</li></Link>
-                    <Link to="/about"><li>About</li></Link>
-                    <Link to="/services"><li>Services</li></Link>
-                    <Link to="/footer"><li>Contact Us</li></Link>
-                </ul>
-            
-                <button className="login-button">Login</button>
-                </div>
-    </nav>
-  );
-};
+    return (
+      <nav className="navbar" id="navbar">
+        <div className="navbar-container">
+          <div className="logo">
+            <img src={logoImage} alt="Invento Logo" className="logo-image" />
+            <span className="logo-text">INVENTO</span>
+          </div>
+  
+          <ul className="nav-links">
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/about">
+              <li>About</li>
+            </Link>
+            <Link to="/services">
+              <li>Services</li>
+            </Link>
+            <Link to="/footer">
+              <li>Contact Us</li>
+            </Link>
+          </ul>
+  
+          <button className="login-button">Login</button>
+        </div>
+      </nav>
+    );
+  }
 
 export default NavBar;
