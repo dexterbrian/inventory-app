78a090f18647e12e951ad0284784c7a89524aea5
Lennie <lennie.mbotela@student.moringaschool.com>
2023-06-12 14:08:37 +0300

    added Deletion logic

--- src/NotificationPopUp.js
+++ src/NotificationPopUp.js
@@ -3,3 +3,13 @@ import React, { useState } from "react";
 const Notification = ({ message, type }) => {
   return <div className={`notification ${type}`}>{message}</div>;
 };
+
+const NotificationPopup = () => {
+    const [notification, setNotification] = useState(null);
+  
+    const handleDeleteProduct = () => {
+      setNotification({ message: 'Product deleted successfully!', type: 'success' });
+      setNotification({ message: 'Error deleting product.', type: 'error' });
+    };
+
+    
