865499adda01db0796351ece11df29b5c938bcce
Sylvia Mumbi <mumbi099@gmail.com>
2023-06-12 12:01:44 +0300

    Added service files

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className='home'>
+       <h1 className='home-title'>Empowering Inventory Management</h1>
+       <h3 className='home-slogan'>Unlock Your Business Potential 
+       with Invento: The Ultimate Inventory Management Solution for Streamlined
+        Operations, Real-Time Insights, and Unwavering Success</h3>
+    </div>
+    
+  )
+}
+
+export default Home;
