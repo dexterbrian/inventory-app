6e05f9e17199978ed28f4ce2e8d24a951bd0d6d9
Sylvia Mumbi <mumbi099@gmail.com>
2023-06-12 15:16:22 +0300

    imported other components into the App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBar from './components/navbar/NavBar';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Service from './components/services/Service';
+import Footer from './components/footer/Footer';
 
 
 function App() {
