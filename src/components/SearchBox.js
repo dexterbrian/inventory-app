6d452769d2f7e3c55946aeaa3641d74f8a9179b3
Sylvia Mumbi <mumbi099@gmail.com>
2023-06-13 06:25:04 +0300

    Edited search box

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import "./SearchBox.css";
 
 const InventorySearchBox = ({ onSearch }) => {
   return (
-    <div>
+    <div className="search-box">
       <input
         type="text"
+        className="search-input"
         placeholder="Search a product name..."
         onChange={ (e) => onSearch(e.target.value) }
       />
-      </div>
+    </div>
   );
 };
 
