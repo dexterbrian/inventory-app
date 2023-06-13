1e4de9ae7edd3fc8d0beff11e3cd9e08247eae3e
Sylvia Mumbi <mumbi099@gmail.com>
2023-06-13 06:02:28 +0300

    General edits

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,50 +1,64 @@
-import { useState } from "react";
-import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./Login.css";
 
-function Login({ setLoggedIn, setNotification }) {
+function Login({ onCancel, setLoggedIn, setNotification }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
 
-    const [ username, setUsername ] = useState('');
-    const [ password, setPassword ] = useState('');
-    const history = useHistory();
+  const onUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
 
-    function onUsernameChange(e) {
-        setUsername(e.target.value);
+  const onPasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onLogin = (e) => {
+    e.preventDefault();
+
+    // Set log in to true if the password and username are correct
+    if (username === "admin" && password === "123456") {
+      setLoggedIn(true);
+      history.push("/");
+    } else {
+      setNotification("Wrong username and password");
     }
 
-    function onPasswordChange(e) {
-        setPassword(e.target.value);
-    }
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
 
-    function onLogin(e) {
-        e.preventDefault();
+  const onCancelClick = () => {
+    onCancel();
+  };
 
-        // set log in to true if password and username is correct
-        if (username === 'admin' && password === '123456') {
-            setLoggedIn(true);
-            history.push('/');
-        } else {
-            setNotification('Wrong username and password');
-        }
-
-        console.log('Username:', username);
-        console.log('Password:', password);
-
-    }
-
-    return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={ onLogin }>
-                <div>
-                <input type="text" name="username" placeholder="Username" onChange={ onUsernameChange } />
-                </div>
-                <div>
-                <input type="password" name="password" placeholder="Password" onChange={ onPasswordChange } />
-                </div>
-                <input type="submit" value="Login" />
-            </form>
+  return (
+    <div className="login-container">
+      <form onSubmit={onLogin}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          onChange={onUsernameChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={onPasswordChange}
+        />
+        <div className="login-buttons">
+          <input type="submit" value="Login" />
+          <button type="button" onClick={onCancelClick}>
+            Cancel
+          </button>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
 
 export default Login;
+
